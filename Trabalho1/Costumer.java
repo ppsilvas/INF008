fd9af8064392647d74bb17079e616dda76c8df7b
Pedro Ferreira <151476266+ppsilvas@users.noreply.github.com>
2024-11-27 00:41:48 +0000

    Substituição para enum de cliente

--- Trabalho1/Costumer.java
+++ Trabalho1/Costumer.java
@@ -3,7 +3,7 @@ package Trabalho1;
 public class Costumer extends User{
     private String adress;
 
-    public Costumer(String name, String email, String password, int type,String adress){
+    public Costumer(String name, String email, String password, Type type, String adress){
         super(name , email, password, type);
         this.adress = adress;
     }
