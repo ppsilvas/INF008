511145818741477b77d2eb49ae4894744a636db7
Pedro Ferreira <151476266+ppsilvas@users.noreply.github.com>
2024-11-27 23:06:55 +0000

    Metodo addCostumer e lista de costumers

--- Trabalho1/Costumer.java
+++ Trabalho1/Costumer.java
@@ -1,10 +1,17 @@
 package Trabalho1;
 
+import java.util.ArrayList;
+
 public class Costumer extends User{
     private String adress;
+    protected ArrayList<Costumer> costumers;
 
     public Costumer(String name, String email, String password, Type type, String adress){
         super(name , email, password, type);
         this.adress = adress;
+    }
+
+    public void addCostumer(Costumer costumer){
+        costumers.add(costumer);
     }
 }
