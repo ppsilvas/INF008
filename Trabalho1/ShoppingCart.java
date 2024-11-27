adc2ce285500b2934db5dc4e709765de5f2ada41
Pedro Ferreira <151476266+ppsilvas@users.noreply.github.com>
2024-11-27 00:44:08 +0000

    Metodos e atributos de ShoppingCart

--- Trabalho1/ShoppingCart.java
+++ Trabalho1/ShoppingCart.java
@@ -1,4 +1,22 @@
 package Trabalho1;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ShoppingCart {
+    private float orderTotal = 0;
+    private ArrayList<Product> inCart;
+
+    public void Add(Product product){
+        inCart.add(product);
+        orderTotal += product.price;
+    }
+
+    public void viewShoppingCart(){
+        int length = inCart.size();
+        Product[] cartList = new Product[length];
+        cartList = inCart.toArray(cartList);
+        System.out.println("Cart items:"+cartList);
+        System.out.println("Order total: "+orderTotal);
+    }
 }
