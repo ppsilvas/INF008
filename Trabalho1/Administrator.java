e3f02dd80409037b36ac97032be7c4c568e9e3bd
Pedro Ferreira <151476266+ppsilvas@users.noreply.github.com>
2024-11-27 00:43:36 +0000

    Criação dos metodos de administrador e seus atributos

--- Trabalho1/Administrator.java
+++ Trabalho1/Administrator.java
@@ -1,23 +1,35 @@
 package Trabalho1;
 
+import java.util.ArrayList;
+
 public class Administrator extends User{
-    public Administrator(String name, String email, String password, int type){
+    protected ArrayList<Product> products;
+    protected ArrayList<Costumer> costumers;
+    protected ArrayList<Administrator> administrators;
+    public Administrator(String name, String email, String password, Type type){
         super(name, email, password, type);
     }
 
-    public void ProductManagement(){
-
+    public void createNewProduct(String name, String description, float price, int numberOfThisProductInStock, String category){
+        Product product = new Product(name, description, price, numberOfThisProductInStock, category);
+        products.add(product);
     }
     
-    public void UsersManagement(){
+    public void createNewUser(String name, String email, String password, Type type, String adress){
+        if(type == Type.Costumer){
+            Costumer costumer = new Costumer(name, email, password, type, adress);
+            costumers.add(costumer);
+        }else{
+            Administrator administrator = new Administrator(name, email, password, type);
+            administrators.add(administrator);
+        }
+    }
+
+    public void moreExpensiveOrderReport(){
 
     }
 
-    public void MostExpensiveOrderReport(){
-
-    }
-
-    public void LowestProductInStock(){
+    public void productWithLowestInventory(){
 
     }
 }
