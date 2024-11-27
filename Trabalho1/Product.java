0969f63edf577333143cbf06c3123f7a5d3ecd76
Pedro Ferreira <151476266+ppsilvas@users.noreply.github.com>
2024-11-27 00:40:40 +0000

    Alteração para protected de price e num do produto em estoque

--- Trabalho1/Product.java
+++ Trabalho1/Product.java
@@ -4,15 +4,17 @@ public class Product {
     private int id;
     private String name;
     private String description;
-    private float price;
-    private int numberOfThisProductsInStock;
+    protected float price;
+    protected int numberOfThisProductsInStock;
+    private String category;
     private int numberOfProducts = 0;
 
-    public Product(String name, String description, float price, int numberOfThisProductInStock){
+    public Product(String name, String description, float price, int numberOfThisProductInStock, String category){
         id = numberOfProducts++;
         this.name = name;
         this.description = description;
         this.price = price;
         this.numberOfThisProductsInStock = numberOfThisProductInStock;
+        this.category = category;
     }
 }
