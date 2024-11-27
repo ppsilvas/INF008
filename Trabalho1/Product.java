2c43bae36912e3665cd913902d8c4c6ec5abf5bb
Pedro Ferreira <151476266+ppsilvas@users.noreply.github.com>
2024-11-27 23:06:25 +0000

    Metodos do produto e lista de produtos

--- Trabalho1/Product.java
+++ Trabalho1/Product.java
@@ -1,5 +1,7 @@
 package Trabalho1;
 
+import java.util.ArrayList;
+
 public class Product {
     private int id;
     private String name;
@@ -8,6 +10,7 @@ public class Product {
     protected int numberOfThisProductsInStock;
     private String category;
     private int numberOfProducts = 0;
+    protected ArrayList<Product> products;
 
     public Product(String name, String description, float price, int numberOfThisProductInStock, String category){
         id = numberOfProducts++;
@@ -16,5 +19,20 @@ public class Product {
         this.price = price;
         this.numberOfThisProductsInStock = numberOfThisProductInStock;
         this.category = category;
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public Product getProduct(int index){
+        return products.get(index);
+    }
+
+    public void productList(){
+        for(int i = 0; i<products.size(); i++){
+            Product product = products.get(i);
+            System.out.println("["+product.id+"] - "+product.name+" - $"+product.price);
+        }
     }
 }
